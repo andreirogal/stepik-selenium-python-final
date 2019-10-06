1f3c824c8f91942765a67f216e2aa0b3f1fe1a87
Andrei Rogal <andreirogal11@gmail.com>
2019-10-06 20:47:19 +0300

    extract go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 
+link = "http://selenium1py.pythonanywhere.com/"
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser):
+    global link
+    browser.get(link)
+    go_to_login_page(browser)
