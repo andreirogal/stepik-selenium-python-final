c10e6f5a9c14be08b9525a62abce5273a779fd21
Andrei Rogal <andreirogal11@gmail.com>
2019-10-08 15:06:28 +0300

    add implicitly_wait to __init__ of BasePage class

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,9 +3,10 @@
 
 class BasePage:
 
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
