c5aff049ec082b7b3cc515c8dc58c1bc77be98ec
Andrei Rogal <andreirogal11@gmail.com>
2019-10-08 15:15:38 +0300

    update base_page and main_page
    BasePage:
    * add is_element_present() method
    Method to exception handling with NoSuchElementException.
    
    MainPage:
    * update should_be_login_link() method
    Add assert user updated is_element_present() method.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from selenium.common.exceptions import NoSuchElementException
+
 
 class BasePage:
 
@@ -10,3 +12,10 @@ class BasePage:
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
