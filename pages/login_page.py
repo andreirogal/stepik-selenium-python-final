5038c3315d94fb4e5357bb6578534b570298da49
Andrei Rogal <andreirogal11@gmail.com>
2019-10-08 21:15:59 +0300

    add LoginPage
    update pages/locators.py:
    * add LoginPageLocators
    
    add asserts for LoginPage

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, \
+            "Url login page is not correct"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), \
+            "Login from is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), \
+            "Register form is not presented"
