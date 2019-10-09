7879f3f241537401550dfae979fddf541d22bc08
Andrei Rogal <andreirogal11@gmail.com>
2019-10-09 12:59:28 +0300

    add solve_quiz_and_get_code() to Base page
    To check that test wrote on Selenium.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+import math
 
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, \
+    NoAlertPresentException
 
 
 class BasePage:
@@ -19,3 +21,17 @@ class BasePage:
         except NoSuchElementException:
             return False
         return True
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
