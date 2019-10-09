14a982965c46acaa5b63492465f813cf8d6bb39c
Andrei Rogal <andreirogal11@gmail.com>
2019-10-09 12:34:15 +0300

    add transition to Login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-from pages.main_page import MainPage
+from pages.main_page import MainPage, LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -9,6 +9,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
