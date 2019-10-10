614cd8768a380fb3c91452b538042e385681aa2c
Andrei Rogal <andreirogal11@gmail.com>
2019-10-10 13:19:59 +0300

    add tests to test_product_page
    - test_guest_cant_see_success_message_after_adding_product_to_basket
    - test_guest_cant_see_success_message
    - test_message_disappeared_after_adding_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -36,3 +36,32 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.should_be_equal_product_name_and_product_added_to_basket()
     page.should_be_equal_product_price_and_basket_value()
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(
+        browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/" \
+           "coders-at-work_207"
+    page = ProductPage(browser=browser, url=link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/" \
+           "coders-at-work_207"
+    page = ProductPage(browser=browser, url=link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/" \
+           "coders-at-work_207"
+    page = ProductPage(browser=browser, url=link)
+    page.open()
+    page.add_to_basket()
+    page.should_be_success_message_disappeared()
