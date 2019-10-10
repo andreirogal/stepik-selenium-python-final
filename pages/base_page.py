e4bd2e8d1d34f802da102f8535c1d93181b7fda6
Andrei Rogal <andreirogal11@gmail.com>
2019-10-10 16:53:11 +0300

    update BasePage
    * Moved methods from MainPage:
    - go_to_login_page()
    - should_be_login_link()
    
    * Update pages/locators.py
    - moved LOGIN_LINK to BasePageLocators
    
    * MainPage is empty now

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,6 +6,7 @@ from selenium.common.exceptions import NoSuchElementException, \
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from .locators import BasePageLocators
 
 
 class BasePage:
@@ -14,6 +15,15 @@ class BasePage:
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(
+            *BasePageLocators.LOGIN_LINK), \
+            "Login link is not presented"
 
     def open(self):
         self.browser.get(self.url)
