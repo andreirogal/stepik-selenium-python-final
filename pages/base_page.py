e426b79b066b1dfc224da63486b98764b55ef45a
Andrei Rogal <andreirogal11@gmail.com>
2019-10-10 13:17:48 +0300

    update BasePage
    - add is_not_element_present()
    - add is_disappeared()

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,6 +3,9 @@ import math
 
 from selenium.common.exceptions import NoSuchElementException, \
     NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class BasePage:
@@ -35,3 +38,21 @@ class BasePage:
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(
+                EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return False
