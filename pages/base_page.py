691bc2364ec735fbbb1ee155e1f88168541baeb6
Andrei Rogal <andreirogal11@gmail.com>
2019-10-11 16:46:18 +0300

    add should_be_authorized_user() to BasePage
    add locator USER_ICON for BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -29,6 +29,11 @@ class BasePage:
             *BasePageLocators.LOGIN_LINK), \
             "Login link is not presented"
 
+    def should_be_authorized_user(self):
+        assert self.is_element_present(
+            *BasePageLocators.USER_ICON), "User icon is not presented," \
+                                          " probably unauthorised user"
+
     def open(self):
         self.browser.get(self.url)
 
--- pages/locators.py
+++ pages/locators.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     BTN_VIEW_BASKET = (By.CSS_SELECTOR, ".btn-group > a[href*='basket']")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators:
